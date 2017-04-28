10637748ed0bca499a98aad0c38ddde70a5c3283
CHEOLJIN <kcj091857@gmail.com>
2017-04-28 19:21:45 +0900

    *web footer view

--- ma-dashboard/src/app/ma-footer/ma-footer.component.ts
+++ ma-dashboard/src/app/ma-footer/ma-footer.component.ts
@@ -6,8 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ma-footer.component.css']
 })
 export class MaFooterComponent implements OnInit {
+  win3win_info = "win3win정보"
+  blog = '블로그';
+  article = "관련기사";
+  api = "API"
+  employ_info = "채용"
+  privacy_policy = "개인정보처리방침";
+  terms = "약관";
+  directory = "디렉터리"
+  language = "언어"
+  company = "win3win ⓒ 2017"
+  constructor() {
 
-  constructor() { }
+  }
 
   ngOnInit() {
   }
